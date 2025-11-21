13c07a14cd21611a4c154edbe66eb75fee52389c
kristheljane <122353366+kristheljane@users.noreply.github.com>
2025-11-21 10:12:30 +0000

    Setup Django REST Framework, start the server, and test the API

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-y)0v67e*mo3)2165e)r$f^or==j-$ce9%6x1v#**-k5wq&c8*@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
